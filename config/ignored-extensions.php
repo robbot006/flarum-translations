cca5d059ce8a89e125b4b33a37b785dd3b6a90d1
Robert Korulczyk <robert@korulczyk.pl>
2026-04-25 14:14:42 +0200

    [1.x] Remove `v17development/flarum-user-badges`

--- config/ignored-extensions.php
+++ config/ignored-extensions.php
@@ -40,6 +40,8 @@ return [
 	'afrux/news-widget',
 	'afrux/online-users-widget',
 	'afrux/top-posters-widget',
+	// migrated to `fof/*`
+	'v17development/flarum-user-badges',
 	/*
 	 * dead/private forks
 	 */
