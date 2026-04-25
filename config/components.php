abdc58c6156d4c330f0052097fac3dbc35b3daf8
Robert Korulczyk <robert@korulczyk.pl>
2026-04-25 12:30:05 +0200

    [1.x] Remove `tohsakarat/post-camera`

--- config/components.php
+++ config/components.php
@@ -1732,12 +1732,6 @@ return [
 			'fr',
 		],
 	],
-	'tohsakarat-post-camera' => [
-		'tag' => 'https://raw.githubusercontent.com/tohsakrat/post-camera/v1.0.2/locale/en.yml',
-		'__builtInLanguages' => [
-			'zh_Hans',
-		],
-	],
 	'tohsakarat-save-and-load-draft-automatically' => [
 		'tag' => 'https://raw.githubusercontent.com/tohsakrat/save-and-load-draft-automatically/v1.10/resources/locale/en.yml',
 		'__builtInLanguages' => [
